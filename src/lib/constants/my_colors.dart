4f69011d5535af6560da22d840cc1a6d5f50eb61
Souhei Koike <sohei-koike@gmo-k.jp>
2023-05-24 10:14:34 +0900

    create file

---
+++ src/lib/constants/my_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color turquoise = const Color.fromARGB(255, 73, 194, 194);
+  static Color tomato = const Color.fromARGB(255, 206, 58, 50);
+  static Color white = const Color.fromARGB(255, 255, 255, 255);
+}
